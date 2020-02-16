a60d3f367271e2ba66af7d659955200dffe25c37
tatsuya <osakana3ex.hahaha@gmail.com>
2020-02-16 16:14:18 +0900

    news apiからフェッチしてコンポーネント描画

--- App.js
+++ App.js
@@ -1,19 +1,51 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, View, FlatList, SafeAreaView } from 'react-native';
+import Constants from 'expo-constants';
+import axios from 'axios';
+import ListItem from './components/ListItem';
+import dummyArticles from './dummies/articles';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!2</Text>
-    </View>
-  );
-}
+const URL = `http://newsapi.org/v2/top-headlines?country=jp&apiKey=${Constants.manifest.extra.newsApiKey}`;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
+
+export default function App() {
+  const [articles, setArticles] = useState([]);
+
+  const fetchArticles = async () => {
+    try {
+      const response = await axios.get(URL);
+      setArticles(response.data.articles);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchArticles();
+  }, []);
+
+  return (
+    // SafeAreaView はM字ハゲの箇所に描画しないようにしてくれるもの
+    <SafeAreaView style={styles.container}>
+      {/* FlatListはscrollViewと比較して、はやい */}
+      <FlatList
+        data={articles}
+        renderItem={({ item }) => (
+          <ListItem
+            imageUrl={item.urlToImage}
+            title={item.title}
+            author={item.author}
+          />
+        )}
+        keyExtractor={(item, index) => index.toString()}
+        // colors="blue"
+      />
+    </SafeAreaView>
+  );
+}
