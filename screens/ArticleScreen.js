9f90097af47a8aa575bf36efc0020fa113814c49
Tanakan7 <ywff34tk@gmail.com>
2020-05-09 06:45:24 +0900

    redux利用

--- screens/ArticleScreen.js
+++ screens/ArticleScreen.js
@@ -1,6 +1,14 @@
 import React from 'react';
-import { View, Text, SafeAreaView, StyleSheet } from 'react-native';
+import {
+  View,
+  Text,
+  SafeAreaView,
+  StyleSheet,
+  TouchableOpacity,
+} from 'react-native';
 import { WebView } from 'react-native-webview';
+import { useDispatch } from 'react-redux';
+import { addClip, deleteClip } from '../store/actions/user';
 
 const styles = StyleSheet.create({
   container: {
@@ -10,8 +18,24 @@ const styles = StyleSheet.create({
 });
 
 export default ArticleScreen = props => {
+  const dispatch = useDispatch();
+
   return (
     <SafeAreaView style={styles.container}>
+      <TouchableOpacity
+        onPress={() =>
+          dispatch(addClip({ clip: props.navigation.getParam('article') }))
+        }
+      >
+        <Text style={{ margin: 10, fontSize: 30 }}>ADD_CLIP</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => {
+          dispatch(deleteClip({ clip: props.navigation.getParam('article') }));
+        }}
+      >
+        <Text style={{ margin: 10, fontSize: 30 }}>DELETE_CLIP</Text>
+      </TouchableOpacity>
       <WebView source={{ uri: props.navigation.getParam('article').url }} />
     </SafeAreaView>
   );
